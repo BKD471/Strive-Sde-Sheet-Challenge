dc7d889e1b09364bd4f416a66a4735743e7c953d
BKD471 <bhaskarkumardas9@gmail.com>
2022-06-17 00:51:06 +0530

    Day 13

--- Strings/reverseWords.cpp
+++ Strings/reverseWords.cpp
@@ -36,3 +36,70 @@ public:
     return res;  
     }
 };
+
+
+
+//  removing the trailing spaces and then  reversing
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+      int n=s.length();
+        
+        
+       
+        int i=0,j=n-1;
+        
+        
+        while(i<n && s[i]==' ') i++;
+        
+        while(j>=0 && s[j]==' ')  j--;
+        
+     
+ 
+        s=s.substr(i,j-i+1);  
+        
+     
+       
+        
+       string res="",temp="";
+       int l=0,h=s.length()-1;
+        
+       int cnt=0;
+        while(l<=h){
+            
+            char ch=s[l];
+            if(s[l]!=' '){
+                temp+=ch;
+                cnt=0;
+            }else if(s[l]==' ' && cnt==0){
+               
+                if(res!=""){
+                    res=temp+" "+res;
+                }else{
+                    res=temp;
+                }
+                temp="";
+                cnt++;
+            }
+            
+            l++;
+        }
+        
+
+        if(temp!=""){
+            if(res!=""){
+                res=temp+" "+res;
+            }else{
+                res=temp;
+            }
+        }
+        
+        
+
+       
+       return res;
+     
+    
+    }
+};

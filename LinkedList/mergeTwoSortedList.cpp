2c1bdbaa230a8bac7b840531db4e2cf18abb7d07
BKD471 <bhaskarkumardas9@gmail.com>
2022-06-19 21:44:07 +0530

    Day 16

--- LinkedList/mergeTwoSortedList.cpp
+++ LinkedList/mergeTwoSortedList.cpp
@@ -10,6 +10,8 @@ using namespace std;
      ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
 
+
+//using recursion
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
@@ -33,6 +35,67 @@ public:
                 cur2->next=mergeTwoLists(cur1,cur2->next);
                 return cur2;
             }
+        
+    }
+};
+
+
+//using iterative approach 
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        
+        ListNode* cur1=list1;
+        ListNode* cur2=list2;
+        
+        ListNode* res=nullptr;
+        ListNode* start;
+        
+        if(!cur1) return cur2;
+        if(!cur2) return cur1;
+        
+        while(cur1 && cur2){
+            
+            if(cur1->val<=cur2->val){
+                
+                if(res==nullptr){
+                      res=cur1;
+                     start=res;
+                }else{
+                    res->next=cur1;
+                    res=res->next;
+                }
+                cur1=cur1->next;
+                
+            }else{
+                
+                if(res==nullptr){
+                       res=cur2;
+                      start=res;
+                }else{
+                    res->next=cur2;
+                    res=res->next;
+                }
+                cur2=cur2->next;
+            }
+            
+        }
+        
+        while(cur1){
+            res->next=cur1;
+            cur1=cur1->next;
+            res=res->next;
+        }
+        
+        while(cur2){
+            res->next=cur2;
+            cur2=cur2->next;
+            res=res->next;
+        }
+        
+        return start;
         
     }
 };
